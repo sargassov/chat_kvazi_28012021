9700982fc89fc4fab58b896bd89d1b1d39943a6c
sargassov <sargassov@yandex.ru>
2021-02-04 06:12:50 +0300

    .

--- server/src/main/java/server/ClientHandler.java
+++ server/src/main/java/server/ClientHandler.java
@@ -55,7 +55,6 @@ public class ClientHandler {
                             }
                         }
                     }
-
                     //цикл работы
                     while (true) {
                         String str = in.readUTF();
